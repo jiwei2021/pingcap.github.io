82fa6a4275b910138ff463ecd9a5052f27f07452
昌维 <867597730@qq.com>
2021-08-24 20:53:04 +0800

    update build script: update convert_html.py

--- scripts/convert_html.py
+++ scripts/convert_html.py
@@ -18,6 +18,8 @@ except NameError:
 abs_hyper_link_pattern = re.compile(r'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}')
 image_rel_src_pattern = re.compile(r'^[\.\/]*media\/')
 
+pingcap_com_blog_or_case_hyper_link_pattern = re.compile(r'https?:\/\/(www\.)?pingcap.com\/(blog|case)-cn\/([-a-zA-Z0-9@:%._\+~#=]{2,256})')
+
 file_path = sys.argv[1]
 folder = sys.argv[2]
 default_version = ''
@@ -31,11 +33,15 @@ with open(file_path, 'r') as f:
 for link in soup.find_all('a'):
     href = link['href']
     if href:
-        if (not abs_hyper_link_pattern.match(href)) and href.rfind('.md') > 0:
+        result = pingcap_com_blog_or_case_hyper_link_pattern.match(href)
+        if result:
+            href = "https://pingcap.com.cn/%s/%s" % (result.groups()[1], result.groups()[2])
+            print("href", href)
+        elif (not abs_hyper_link_pattern.match(href)) and href.rfind('.md') > 0:
             href = href.replace('.md', '')
             href = re.sub(r'^[\.\/]*', '/', href, count=0, flags=0)
             href = os.path.normpath('/' + folder + default_version + '/' + href)
-            link['href'] = href
+        link['href'] = href
 
 for img in soup.find_all('img'):
     src = img['src']
